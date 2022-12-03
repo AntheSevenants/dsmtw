7619b8d87ff08479bacbffa7ad88b090e8e3456d
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-12-03 19:14:37 +0100

    Allow hosting of external resources

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,7 @@
 import os
 import random
 
-from flask import session, redirect, url_for, render_template, request, send_from_directory
+from flask import session, redirect, url_for, render_template, request, send_file
 from . import main
 
 from .. import global_questions_directory
@@ -10,3 +10,12 @@ from .. import global_questions_directory
 @main.route('/overview')
 def overview(callback=None):
 	return render_template('overview.html')
+
+@main.route('/resources/<string:round_text>/<string:filename>')
+def display_label_image(round_text, filename):
+	if not os.path.isabs(global_questions_directory):
+		path = os.path.join("..", global_questions_directory, round_text, filename)
+	else:
+		path = os.path.join(global_questions_directory, round_text, filename)
+
+	return send_file(path)
