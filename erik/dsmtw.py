7eab34df3a43c276bd69f8b0a752e6f8c785a24d
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-12-03 15:04:43 +0100

    Initialise gameshow parent class

--- erik/dsmtw.py
+++ erik/dsmtw.py
@@ -4,5 +4,8 @@ import os.path
 from gameshow.gameshow import Gameshow
 
 class DeSlimsteMens(Gameshow):
-	def __init__(self):
-		pass
+	def __init__(self, no_players):
+		rounds = [ "3-6-9", "Open deur", "Puzzel", "Galerij", "Collectief geheugen",
+				   "Finale" ]
+
+		super().__init__("De Slimste Mens Ter Wereld", rounds, no_players)
