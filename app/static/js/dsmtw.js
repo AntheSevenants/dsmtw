a770daa3a1740291baf18a073f4b0192e37e9166
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-12-03 16:50:08 +0100

    Build score panels dynamically

--- app/static/js/dsmtw.js
+++ app/static/js/dsmtw.js
@@ -3,9 +3,15 @@ class DeSlimsteMens extends Gameshow {
 		super();
 
 		this._currentSubroundText = null;
+
+		this.scoreDomBuilt = false;
 	}
 
 	renderState(state) {
+		if (!this.scoreDomBuilt) {
+			this.buildScoreDom(state);
+		}
+
 		super.renderState(state);
 
 		document.getElementById("currentround").innerHTML = state.current_round_text;
@@ -26,6 +32,48 @@ class DeSlimsteMens extends Gameshow {
 
 	renderStateGame(state) {
 
+	}
+
+	// Create the DOM for the scores of the players
+	// Cannot be hardcoded anymore, since here in the future, 
+	// we have variable player counts
+	buildScoreDom(state) {
+		for (let i = 0; i < state.players.length; i++) {
+			let player = state.players[i];
+
+			let circleBox = document.createElement("div");
+			circleBox.className = "circlebox";
+
+			let playerName = document.createElement("div")
+			playerName.className = "name";
+			playerName.innerHTML = player.name;
+
+			let innerCircle = document.createElement("div");
+			innerCircle.className = "circle";
+			innerCircle.id = this.getCircleElementName(i);
+
+			let score = document.createElement("p");
+			score.className = "score";
+			score.id = this.getScoreElementName(i);
+
+			let background = document.createElement("div");
+			background.className = "background";
+
+			circleBox.appendChild(playerName);
+			circleBox.appendChild(innerCircle);
+			innerCircle.appendChild(score);
+			innerCircle.appendChild(background);
+
+			document.getElementById("scores").appendChild(circleBox);
+		}
+	}
+
+	getCircleElementName(playerIndex) {
+		return `circle_player_${playerIndex}`;
+	}
+
+	getScoreElementName(playerIndex) {
+		return `score_player_${playerIndex}`;
 	}
 }
 
