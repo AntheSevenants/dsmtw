595d728cfdfae0077eb2dab340e17f922785d10a
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-12-12 22:38:27 +0100

    Add ability to stop sounds

--- app/static/js/misc/Sound.js
+++ app/static/js/misc/Sound.js
@@ -1,5 +1,20 @@
 class Sound {
 	static playSound(soundName) {
-		document.getElementById(`snd_${soundName}`).play();
+		Sound.playStopSound(soundName, true);
+	}
+
+	static stopSound(soundName) {
+		Sound.playStopSound(soundName, false);
+	}
+
+	static playStopSound(soundName, play) {
+		let soundElement = document.getElementById(`snd_${soundName}`);
+
+		if (play) {
+			soundElement.currentTime = 0;
+			soundElement.play();
+		} else {
+			soundElement.stop();
+		}
 	}
 }
